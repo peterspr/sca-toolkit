b5d8c0d26cab4f22c4532e899dbca282746a6175
Jadon Procter <58637300+jadonprocter@users.noreply.github.com>
2023-03-05 18:48:26 +0000

    Signal to noise (#34)
    * SNR updated to new file format, performs on single tile. -- untested atm.
    
    * added comments to SNR and created property snr which calls calculate if needed and exposes the self.snr
    
    * started writing tests for SNR.
    
    * declare p_byte outside of second loop.
    
    * added basic plotting abilities of passed value.

--- src/notscared/preprocessing/SNR.py
+++ src/notscared/preprocessing/SNR.py
@@ -1,44 +1,88 @@
 import numpy as np
-from src.notscared.statistics.welford import Welford
-import fire
+import matplotlib.pyplot as plt
+import random
+
+"""
+    1. Take in a plaintxt with samples
+    2. Get a ptxt with byte 0x00 in the 0th place make a bucket
+    3. Do step 2 for each ptxt/sample with 0x01 -> 0xFF in 0th place
+    4. Update mean and variance in each bucket as an np.array 
+    """
+
 
 class SNR:
-    def __init__(self):
-        self._size = 0
-        self._welfords = None
+
+    def __init__(self, n_traces: int):
+        # declare an array of 256 (0x00 -> 0xFF), number of traces in a sample, and then [0] == mean and [1] == variance
+        self._accumulator = np.empty((256, n_traces, 2))
+        # declare n_traces to be accessed by self
+        self.n_traces = n_traces
+
         self._snr = None
 
-    def push(self, sample):
-        if self._size == 0:
-            self._size += sample.size
-            self._welfords = [Welford() for _ in range(self._size)]
-            self._snr = np.empty(self._size)
-        else:
-            if self._size < sample.size:
-                for _ in range(sample.size - self._size):
-                    self._welfords.append(Welford())
-                self._size += sample.size - self._size
-                self._snr.reshape(self._size)
+    def push(self, ptxts: np.ndarray, samples: np.ndarray):
+        """
+        input: takes in an array of plaintexts and an array of samples
+        output: None, updates accumulator values.
+        description: takes in plaintext array and samples array and calculates the accumulator values of 
+            mean and variance based on the first byte value and trace point in time. (vertically)
 
-        for index in range(self._size):
-            self._welfords[index].push(sample[index])
+        """
+        try:
+            # for each ptxt in
+            for ptxt in range(ptxts.shape[0]):
+                p_byte = int(ptxts[ptxt][0])  # first byte of ptxt
+                # for each trace
+                for trace_index in range(self.n_traces):
+                    # index in by first byte value, trace value (vertical column), mean value access OR variance value access
+                    current_mean = self._accumulator[p_byte][trace_index][0]
+                    current_S = self._accumulator[p_byte][trace_index][1]
+
+                    # get the new mean by adding (xi - x_mean)/n
+                    new_mean = current_mean + (samples[ptxt][trace_index] - current_mean) * 1.0 / self.n_traces
+                    # get new S by multiplying (xi - pop_mean) and (xi - sample_mean) and adding it to current_S
+                    new_S = current_S + (samples[ptxt][trace_index] - current_mean) * (
+                            samples[ptxt][trace_index] - new_mean)
+
+                    # set accumulator values for next loop.
+                    self._accumulator[p_byte][trace_index][0] = new_mean
+                    self._accumulator[p_byte][trace_index][1] = new_S
+
+        except IndexError as e:
+            print(f"Failed to push... {e}")
+
+    def calculate(self):
+
+        try:
+            # vertorize divide with lambda function on vertical variances
+            apply_variance = lambda x: x / self.n_traces
+            apply_variance(self._accumulator[:][:][1])
+
+            # the Vertical Variance of the Vertical Means
+            signal = np.var(self._accumulator[:][:][0])
+
+            # the Vertical Mean of the Vertical Variance
+            noise = np.mean(self._accumulator[:][:][1])
+
+            # return SNR
+            self._snr = signal / noise
+
+        except IndexError as e:
+            print(f"Failed to Calculate... {e}")
+
+
+    def plot_snr(self, plot_values):
+        s = self.snr
+        vertical_plot_values = np.stack(plot_values, axis=-1)
+        plt.style.use("_mpl-gallery")
+        for value in vertical_plot_values:
+            plt.plot(value[value > s], color="r")
+            plt.plot(value[value < s], color="b")
+        plt.show()
 
     @property
     def snr(self):
-        for index, w in enumerate(self._welfords):
-            signal = w.mean
-            noise = w.std_dev
-            self._snr[index] = signal / noise
+        if self._snr is None:
+            self.calculate()
         return self._snr
-
-    def get_features_of_sample(self, sample):
-        return sample[sample >= [w.mean for w in self._welfords]]
-
-    def plot_signal_vs_noise(self, samples):
-        pass
-
-
-if __name__ == '__main__':
-    fire.Fire(SNR)
-
 
